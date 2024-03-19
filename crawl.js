7e5f87413cd518327b9680a7f1b799d1e186f9ee
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-03-19 15:58:08 +0100

    add getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require("jsdom");
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
@@ -8,6 +10,34 @@ function normalizeURL(url) {
   return fullPath;
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const aElements = dom.window.document.querySelectorAll("a");
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0, 1) === "/") {
+      // relative
+      try {
+        const urlObj = new URL(aElement.href, baseURL);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.error(`${err.message}: ${aElement.href}`);
+      }
+    } else {
+      // absolute
+      try {
+        const urlObj = new URL(aElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.error(`${err.message}: ${aElement.href}`);
+      }
+    }
+  }
+
+  return urls;
+}
+
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
